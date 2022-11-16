3cff3512c9c53030d31d380394c7aa5e36c1c2cb
PIYUJIN <yjyj020201@gmail.com>
2022-11-16 20:51:24 +0900

    Add calendar in Household Main Page
    Use CalendarView

--- app/src/main/java/com/example/babygage_ocr/HouseholdmainActivity.kt
+++ app/src/main/java/com/example/babygage_ocr/HouseholdmainActivity.kt
@@ -3,6 +3,11 @@ package com.example.babygage_ocr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.babygage_ocr.databinding.ActivityHouseholdmainBinding
+import android.widget.CalendarView
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.Month
+import java.util.*
 
 class HouseholdmainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +16,19 @@ class HouseholdmainActivity : AppCompatActivity() {
         binding = ActivityHouseholdmainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        //날짜 형태
+        val dateform : DateFormat = SimpleDateFormat("yyyy년 MM월 dd일")
+        //date type (오늘 날짜)
+        val date : Date = Date(binding.calendarView.date)
+
+        binding.date.text = dateform.format(date)
+
+        binding.calendarView.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
+            var day : String = "${year}년 ${month+1}월 ${dayOfMonth}일"
+
+            binding.date.text = day
+        }
 
     }
 }
