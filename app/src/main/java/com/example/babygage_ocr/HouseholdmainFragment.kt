cbdc790727eae657584a95b190ed8db8878525d6
PIYUJIN <yjyj020201@gmail.com>
2022-12-04 17:36:03 +0900

    complete diary function
    change diary text depending on user and date

--- app/src/main/java/com/example/babygage_ocr/HouseholdmainFragment.kt
+++ app/src/main/java/com/example/babygage_ocr/HouseholdmainFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context.MODE_NO_LOCALIZED_COLLATORS
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -48,6 +49,7 @@ class HouseholdmainFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -55,6 +57,7 @@ class HouseholdmainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHouseholdmainBinding.inflate(inflater, container, false)
+
         setUpView()
 
 
@@ -62,38 +65,12 @@ class HouseholdmainFragment : Fragment() {
         return binding.root
     }
     private fun setUpView() {
-//        binding.date.bringToFront()
-//        //날짜 형태
-//        val dateform: DateFormat = SimpleDateFormat("yyyy년 MM월 dd일")
-//        //date type (오늘 날짜)
-//        val date: Date = Date(binding.calendarView.date)
-//
-//        binding.date.text = dateform.format(date)
-//
-//        binding.calendarView.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
-//            var day: String = "${year}년 ${month + 1}월 ${dayOfMonth}일"
-//
-//            binding.date.text = day
-//
-//        }
 
 
         firebaseAuth= FirebaseAuth.getInstance()
         val currentUser = firebaseAuth?.currentUser
         firestore= FirebaseFirestore.getInstance()
         binding.date.bringToFront()
-
-
-        if(binding.diary.getText() == "") {
-            binding.savebtn.visibility = View.VISIBLE // 저장 버튼이 Visible
-            binding.editbtn.visibility = View.INVISIBLE // 수정 Button이 Invisible
-            binding.deletebtn.visibility = View.INVISIBLE // 삭제 Button이 Invisible
-        }
-        else {
-            binding.savebtn.visibility = View.INVISIBLE
-            binding.editbtn.visibility = View.VISIBLE
-            binding.deletebtn.visibility = View.VISIBLE
-        }
 
         //날짜 형태
         val dateform: DateFormat = SimpleDateFormat("yyyy년 MM월 dd일")
@@ -102,56 +79,105 @@ class HouseholdmainFragment : Fragment() {
 
         binding.date.text = dateform.format(date)
 
-        binding.calendarView.setOnDateChangeListener { calendarView, year, month, dayOfMonth ->
+        Log.d("ITM",binding.date.text.toString())
+        val docRef = firestore.collection("${firebaseAuth.currentUser!!.uid.toString()} diary").document(binding.date.text.toString())
+        docRef.get()
+            .addOnSuccessListener { document ->
+                Log.d("ITM", document.data.toString())
+                if(document.data?.get("date")==binding.date.text.toString()) {
+                    binding.diary.text = document.data?.get("diary").toString()
+                    binding.diaryEditTxt.setText(document.data?.get("diary").toString())
+                    binding.savebtn.visibility = View.INVISIBLE
+                    binding.editbtn.visibility = View.VISIBLE
+                    binding.deletebtn.visibility = View.VISIBLE
+                    binding.diaryEditTxt.visibility = View.INVISIBLE
+                    binding.diary.visibility = View.VISIBLE
+                }
+                else {
+                    Log.d("ITM", "No such document")
+                    binding.diary.text = null
+                    binding.diaryEditTxt.setText(null)
+                    binding.savebtn.visibility = View.VISIBLE // 저장 버튼이 Visible
+                    binding.editbtn.visibility = View.INVISIBLE // 수정 Button이 Invisible
+                    binding.deletebtn.visibility = View.INVISIBLE // 삭제 Button이 Invisible
+                    binding.diaryEditTxt.visibility = View.VISIBLE
+                    binding.diary.visibility = View.INVISIBLE
+                }
+            }
+
+        binding.calendarView.setOnDateChangeListener { calendarView, year : Int, month : Int, dayOfMonth : Int ->
             var day: String = "${year}년 ${month + 1}월 ${dayOfMonth}일"
 
             binding.date.text = day
-
-            if(binding.diary.getText() == "") {
-                binding.savebtn.visibility = View.VISIBLE // 저장 버튼이 Visible
-                binding.diaryEditTxt.visibility = View.VISIBLE // EditText가 Visible
-                binding.diary.visibility = View.INVISIBLE // 저장된 일기 textView가 Invisible
-                binding.editbtn.visibility = View.INVISIBLE // 수정 Button이 Invisible
-                binding.deletebtn.visibility = View.INVISIBLE // 삭제 Button이 Invisible
-
-            }
-            else {
-                binding.savebtn.visibility = View.INVISIBLE
-                binding.diaryEditTxt.visibility = View.INVISIBLE
-                binding.diary.visibility = View.VISIBLE
-                binding.editbtn.visibility = View.VISIBLE
-                binding.deletebtn.visibility = View.VISIBLE
-            }
-//            binding.diaryEditTxt.setText("") // EditText에 공백값 넣기
-
-            checkedDay(year, month, dayOfMonth) // checkedDay 메소드 호출
+            val docRef = firestore.collection("${firebaseAuth.currentUser!!.uid.toString()} diary").document(binding.date.text.toString())
+            docRef.get()
+                .addOnSuccessListener { document ->
+                    if(document.data?.get("date")==binding.date.text.toString()) {
+                        binding.diary.text = document.data?.get("diary").toString()
+                        binding.diaryEditTxt.setText(document.data?.get("diary").toString())
+                        binding.savebtn.visibility = View.INVISIBLE
+                        binding.editbtn.visibility = View.VISIBLE
+                        binding.deletebtn.visibility = View.VISIBLE
+                        binding.diaryEditTxt.visibility = View.INVISIBLE
+                        binding.diary.visibility = View.VISIBLE
+                    }
+                    else {
+                        Log.d("ITM", "No such document")
+                        binding.diary.text = null
+                        binding.diaryEditTxt.setText(null)
+                        binding.savebtn.visibility = View.VISIBLE // 저장 버튼이 Visible
+                        binding.editbtn.visibility = View.INVISIBLE // 수정 Button이 Invisible
+                        binding.deletebtn.visibility = View.INVISIBLE // 삭제 Button이 Invisible
+                        binding.diaryEditTxt.visibility = View.VISIBLE
+                        binding.diary.visibility = View.INVISIBLE
+                    }
+                }
         }
 
         binding.savebtn.setOnClickListener { // save Button 클릭
-            str = binding.diaryEditTxt.getText().toString() // str 변수에 edittext 내용을 String형으로 저장
-            binding.diary.text = "${str}" // textView에 str 출력
-            binding.savebtn.visibility = View.INVISIBLE
-            binding.editbtn.visibility = View.VISIBLE
-            binding.deletebtn.visibility = View.VISIBLE
-            binding.diaryEditTxt.visibility = View.INVISIBLE
-            binding.diary.visibility = View.VISIBLE
-            var userDiary = UserDiary()
-            userDiary.uid = firebaseAuth.currentUser!!.uid
-            userDiary.useId = firebaseAuth.currentUser!!.email
-            userDiary.diary = binding.diary.text.toString()
-            userDiary.date = binding.date.text.toString()
-            firestore?.collection("${firebaseAuth.currentUser!!.uid.toString()} diary")?.document(binding.date.text.toString())?.set(userDiary)
-            Toast.makeText(getActivity(),"save diary",Toast.LENGTH_SHORT).show()
+            val docRef = firestore.collection("${firebaseAuth.currentUser!!.uid.toString()} diary").document(binding.date.text.toString())
+            docRef.get()
+                .addOnSuccessListener { document ->
+                    if(document.data?.get("date")==binding.date.text.toString()) {
+                        Log.d("ITM", "DocumentSnapshot data: ${document.data}")
+                        Log.d("ITM", "DocumentSnapshot data: ${document.data?.get("date")}")
+                        binding.diary.text = binding.diaryEditTxt.text.toString()
+                        var userDiary = UserDiary()
+                        userDiary.uid = firebaseAuth.currentUser!!.uid
+                        userDiary.useId = firebaseAuth.currentUser!!.email
+                        userDiary.diary = binding.diary.text.toString()
+                        userDiary.date = binding.date.text.toString()
+                        firestore?.collection("${firebaseAuth.currentUser!!.uid.toString()} diary")?.document(binding.date.text.toString())?.set(userDiary)
+                        Toast.makeText(getActivity(),"save diary",Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        Log.d("ITM", "No such document")
+                        binding.diary.text = binding.diaryEditTxt.text.toString()
+                        var userDiary = UserDiary()
+                        userDiary.uid = firebaseAuth.currentUser!!.uid
+                        userDiary.useId = firebaseAuth.currentUser!!.email
+                        userDiary.diary = binding.diary.text.toString()
+                        userDiary.date = binding.date.text.toString()
+                        firestore?.collection("${firebaseAuth.currentUser!!.uid.toString()} diary")?.document(binding.date.text.toString())?.set(userDiary)
+                        Toast.makeText(getActivity(),"save diary",Toast.LENGTH_SHORT).show()
+                    }
+                    binding.savebtn.visibility = View.INVISIBLE
+                    binding.editbtn.visibility = View.VISIBLE
+                    binding.deletebtn.visibility = View.VISIBLE
+                    binding.diaryEditTxt.visibility = View.INVISIBLE
+                    binding.diary.visibility = View.VISIBLE
+                }
+                .addOnFailureListener { exception ->
+                    Log.d("ITM", "get failed with ", exception)
+                }
         }
 
         binding.editbtn.setOnClickListener { // 수정 버튼을 누를 시
             binding.diaryEditTxt.visibility = View.VISIBLE
             binding.diary.visibility = View.INVISIBLE
-            binding.diaryEditTxt.setText(str) // editText에 textView에 저장된 내용을 출력
             binding.savebtn.visibility = View.VISIBLE
             binding.editbtn.visibility = View.INVISIBLE
             binding.deletebtn.visibility = View.INVISIBLE
-            binding.diary.text = "${binding.diaryEditTxt.getText()}"
         }
 
         binding.deletebtn.setOnClickListener {
@@ -172,46 +198,6 @@ class HouseholdmainFragment : Fragment() {
         })
         )
     }
-
-
-    fun checkedDay(cYear: Int, cMonth: Int, cDay: Int) {
-        fname = "" + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"  // 저장할 파일 이름 설정. Ex) 2019-01-20.txt
-        var fis: FileInputStream? = null // FileStream fis 변수 설정
-
-        try {
-            fis = getActivity()?.openFileInput(fname) // fname 파일 오픈!!
-
-            val fileData = ByteArray(fis!!.available()) // fileData에 파이트 형식으로 저장
-            fis.read(fileData) // fileData를 읽음
-            fis.close()
-
-            str = String(fileData) // str 변수에 fileData를 저장
-
-            binding.diaryEditTxt.visibility = View.INVISIBLE
-            binding.diary.visibility = View.VISIBLE
-            binding.diary.text = "${str}" // textView에 str 출력
-
-            binding.savebtn.visibility = View.INVISIBLE
-            binding.editbtn.visibility = View.VISIBLE
-            binding.deletebtn.visibility = View.VISIBLE
-
-
-
-            if(binding.diary.getText() == ""){
-                binding.diary.visibility = View.INVISIBLE
-//                diaryTextView.visibility = View.VISIBLE
-                binding.savebtn.visibility = View.VISIBLE
-                binding.editbtn.visibility = View.INVISIBLE
-                binding.deletebtn.visibility = View.INVISIBLE
-                binding.diaryEditTxt.visibility = View.VISIBLE
-            }
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-
 
 
 
